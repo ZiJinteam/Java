8b79be34db1fecafc7e3e5f1e60be1aeeb5ec91e
MatsuSakura <lexingtonqy@outlook.com>
2021-05-21 13:59:45 +0800

    1

---
+++ src/test.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+public class test {
+    private static Connection dbConn = null;
+
+    public static void main(String[] args) {
+        String dbURL = "jdbc:sqlserver://127.0.0.1:1433;DatabaseName=test";
+
+        try {
+            //1.加载驱动
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            System.out.println("加载驱动成功！");
+            //2.连接
+            dbConn = DriverManager.getConnection(dbURL, "sa", "123");
+            System.out.println("连接数据库成功！");
+            String sql="select * from users";
+            PreparedStatement statement=null;
+            statement=dbConn.prepareStatement(sql);
+            ResultSet res=null;
+            res=statement.executeQuery();
+            while(res.next()){
+                String title=res.getString("password");
+                System.out.println(title);
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("连接数据库失败！");
+        }
+
+    }
+}
